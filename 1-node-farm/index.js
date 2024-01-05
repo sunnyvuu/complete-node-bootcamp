23ced3460a8f3275956fc7318c183d4c43d2bd51
Madeline Yi <sunhunfunbun@gmail.com>
2024-01-05 16:21:45 -0600

    started working on server stuff

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+
+/////////////////////////////////////////////
+// FILES
 
 // Blocking, sync way
 // const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
@@ -11,23 +15,33 @@ const fs = require("fs");
 // console.log("File Written");
 
 // Non-blocking, sync way
-fs.readFile("./starter/txt/start.txt", "utf-8", (err, data1) => {
-  if (err) console.log(`Error: ${err}`);
+// fs.readFile("./starter/txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) console.log(`Error: ${err}`);
 
-  fs.readFile(`./starter/txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./starter/txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./starter/txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./starter/txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile(
-        "./starter/txt/final.txt",
-        `${data2} \n ${data3}`,
-        "utf-8",
-        (err) => {
-          console.log("File has been written");
-        }
-      );
-    });
-  });
+//       fs.writeFile(
+//         "./starter/txt/final.txt",
+//         `${data2} \n ${data3}`,
+//         "utf-8",
+//         (err) => {
+//           console.log("File has been written");
+//         }
+//       );
+//     });
+//   });
+// });
+// console.log("Will Read file!");
+
+/////////////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
 });
-console.log("Will Read file!");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
